3cadbc6cb33bcbc1827c4d2dcc59ae2afd5955ba
Fellipy Santos Albuquerque <fellipy.santos@gmail.com>
2021-07-16 19:43:50 -0300

    Atividade Modulo1

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,46 @@
+import { Button } from './Button';
+import { api } from '../services/api';
+import { useEffect, useState } from 'react';
+
+import '../styles/sidebar.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SideBarProps{
+  selectedGenreId: number;
+  onChangeGenre(id: number): void;
+
+}
+export function SideBar({ selectedGenreId, onChangeGenre}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+
+  return <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => onChangeGenre(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+}
